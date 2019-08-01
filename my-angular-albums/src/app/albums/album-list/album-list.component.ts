28c6ce752043c6b643b88b92dd962962940df465
prukhlya <prukhlya@gmail.com>
2019-08-01 12:05:47 -0400

    8-2

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
+import { AlbumService } from "../shared/album.service"
 import { Album } from "../album.model";
-import { ALBUMS } from "../albums.data";
-import { AlbumService } from '../shared/album.service';
 
 @Component({
   selector: "app-album-list",
@@ -11,21 +10,21 @@ import { AlbumService } from '../shared/album.service';
 })
 export class AlbumListComponent implements OnInit {
   albumsArray: Album[];
-  constructor(private albumService: AlbumService) { };
+
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit(): void {
     this.getAlbums();
+   }
+
+   parentFunctionHandler(album) {
+    alert('Album ' + album.albumName + ' was sent from the album card component');
   }
 
   getAlbums() {
-    this.albumsArray = this.albumService.getAlbums();
-  }
-
-
-
-  parentFunctionHandler(album) {
-    alert(
-      "Album " + album.albumName + " was sent from the album card component"
-    );
+    this.albumService.getAlbums()
+    .subscribe(
+        albums => this.albumsArray = albums,
+        error => console.log("Error: ", error));
   }
 }
