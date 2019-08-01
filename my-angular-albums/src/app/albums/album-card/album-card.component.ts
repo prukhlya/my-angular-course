31a94d4d5c6c93cba3594f3fa4aafaeb1daed7fc
prukhlya <prukhlya@gmail.com>
2019-08-01 09:50:09 -0400

    ch6 pipes

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { Album } from '../album.model';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Album } from "../album.model";
 
 @Component({
-  selector: 'app-album-card',
-  templateUrl: './album-card.component.html',
-  styleUrls: ['./album-card.component.css']
+  selector: "app-album-card",
+  templateUrl: "./album-card.component.html",
+  styleUrls: ["./album-card.component.css"]
 })
-
 export class AlbumCardComponent implements OnInit {
   @Input()
-    album: Album;
-  constructor() { }
+  album: Album;
 
   @Output()
-    albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+  albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+
+  newPrice: number;
 
   showAlbum() {
-        this.albumClicked.emit(this.album);
+    this.albumClicked.emit(this.album);
   }
 
   ngOnInit() {
+    // Check if the album is on sale
+    if (this.album.onSale)
+      // Apply 10% discount
+      this.newPrice = this.album.price - (this.album.price * .10);
   }
-
 }
--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Album } from '../album.model';
+import { Component, OnInit } from "@angular/core";
+
+import { Album } from "../album.model";
 import { ALBUMS } from "../albums.data";
 
 @Component({
-  selector: 'app-album-list',
-  templateUrl: './album-list.component.html',
-  styleUrls: ['./album-list.component.css']
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.css"]
 })
 export class AlbumListComponent implements OnInit {
   albumsArray: Album[];
-  constructor() { }
 
   ngOnInit(): void {
-    this.albumsArray = this.albumsArray = ALBUMS;
-   }
-parentFunctionHandler(album) {
-  alert('Album ' + album.albumName + ' was sent from the album card component');
-}
+    this.albumsArray = ALBUMS;
+  }
 
+  parentFunctionHandler(album) {
+    alert(
+      "Album " + album.albumName + " was sent from the album card component"
+    );
+  }
 }
