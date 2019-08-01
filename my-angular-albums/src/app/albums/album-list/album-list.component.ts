9c8c881b40ba26553cb5f42f26412ea266ce95f5
prukhlya <prukhlya@gmail.com>
2019-08-01 10:41:17 -0400

    ch8-1

--- my-angular-albums/src/app/albums/album-list/album-list.component.ts
+++ my-angular-albums/src/app/albums/album-list/album-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { Album } from "../album.model";
 import { ALBUMS } from "../albums.data";
+import { AlbumService } from '../shared/album.service';
 
 @Component({
   selector: "app-album-list",
@@ -10,10 +11,17 @@ import { ALBUMS } from "../albums.data";
 })
 export class AlbumListComponent implements OnInit {
   albumsArray: Album[];
+  constructor(private albumService: AlbumService) { };
 
   ngOnInit(): void {
-    this.albumsArray = ALBUMS;
+    this.getAlbums();
   }
+
+  getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
+
+
 
   parentFunctionHandler(album) {
     alert(
